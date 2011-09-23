40ecc40b538fcee6857875f1eaee39ee1cf37221
Monthadar Al Jaberi <monthadar@gmail.com>
2011-09-23 13:46:34 +0200

    Style clean up by Adrian.

--- src/if_medium.c
+++ src/if_medium.c
@@ -31,74 +31,88 @@
 #include "if_wtapvar.h"
 #include "if_medium.h"
 
-void init_medium(struct wtap_medium *md)
+void
+init_medium(struct wtap_medium *md)
 {
-      DWTAP_PRINTF("%s\n", __func__);
-      STAILQ_INIT(&md->md_pktbuf);
-      mtx_init(&md->md_mtx, "wtap_medium mtx", NULL, MTX_DEF | MTX_RECURSE);
-      
-      // Event handler for sending packets between wtaps
-      struct eventhandler *eh = (struct eventhandler *)malloc(sizeof(struct eventhandler), M_WTAP, M_NOWAIT | M_ZERO);
-      eh->tq = taskqueue_create("wtap_tx_taskq",  M_NOWAIT | M_ZERO,
-		taskqueue_thread_enqueue, &eh->tq);
-      taskqueue_start_threads(&eh->tq, 1, PI_NET, "%s taskq", "wtap_medium");
-      md->tx_handler = eh;
-      md->open = 0; // medium closed by default
+
+	DWTAP_PRINTF("%s\n", __func__);
+	STAILQ_INIT(&md->md_pktbuf);
+	mtx_init(&md->md_mtx, "wtap_medium mtx", NULL, MTX_DEF | MTX_RECURSE);
+
+	/* Event handler for sending packets between wtaps */
+	struct eventhandler *eh = (struct eventhandler *)
+	    malloc(sizeof(struct eventhandler), M_WTAP, M_NOWAIT | M_ZERO);
+	eh->tq = taskqueue_create("wtap_tx_taskq",  M_NOWAIT | M_ZERO,
+	    taskqueue_thread_enqueue, &eh->tq);
+	taskqueue_start_threads(&eh->tq, 1, PI_NET, "%s taskq", "wtap_medium");
+	md->tx_handler = eh;
+	/* Mark medium closed by default */
+	md->open = 0;
 }
 
-void deinit_medium(struct wtap_medium *md)
+void
+deinit_medium(struct wtap_medium *md)
 {
-      DWTAP_PRINTF("%s\n", __func__);
-      taskqueue_free(md->tx_handler->tq);
-      free(md->tx_handler, M_WTAP);
+
+	DWTAP_PRINTF("%s\n", __func__);
+	taskqueue_free(md->tx_handler->tq);
+	free(md->tx_handler, M_WTAP);
 }
- 
-int medium_transmit(struct wtap_medium *md, int id, struct mbuf*m)
+
+int
+medium_transmit(struct wtap_medium *md, int id, struct mbuf*m)
 {
-      mtx_lock(&md->md_mtx);
-      if(md->open == 0){
-	    DWTAP_PRINTF("[%d] dropping m=%p\n", id, m);
-	    m_free(m);
-	    mtx_unlock(&md->md_mtx);
-	    return 0;
-      }
-      
-      DWTAP_PRINTF("[%d] transmiting m=%p\n", id, m);
-      struct packet *p = (struct packet *)malloc(sizeof(struct packet), M_WTAP_PACKET, M_ZERO | M_NOWAIT);
-      p->id = id;
-      p->m = m;
-      
-      STAILQ_INSERT_TAIL(&md->md_pktbuf, p, pf_list);
-      taskqueue_enqueue(md->tx_handler->tq, &md->tx_handler->proc);
-      mtx_unlock(&md->md_mtx);
-      
+
+	mtx_lock(&md->md_mtx);
+	if (md->open == 0){
+		DWTAP_PRINTF("[%d] dropping m=%p\n", id, m);
+		m_free(m);
+		mtx_unlock(&md->md_mtx);
+		return 0;
+	}
+
+	DWTAP_PRINTF("[%d] transmiting m=%p\n", id, m);
+	struct packet *p = (struct packet *)malloc(sizeof(struct packet),
+	    M_WTAP_PACKET, M_ZERO | M_NOWAIT);
+	p->id = id;
+	p->m = m;
+
+	STAILQ_INSERT_TAIL(&md->md_pktbuf, p, pf_list);
+	taskqueue_enqueue(md->tx_handler->tq, &md->tx_handler->proc);
+	mtx_unlock(&md->md_mtx);
+
       return 0;
 }
 
-struct packet *medium_get_next_packet(struct wtap_medium *md)
+struct packet *
+medium_get_next_packet(struct wtap_medium *md)
 {
-      struct packet *p;
-      mtx_lock(&md->md_mtx);
-      p = STAILQ_FIRST(&md->md_pktbuf);
-      if(p == NULL){
-	  mtx_unlock(&md->md_mtx);
-	  return NULL;
-      }
-      STAILQ_REMOVE_HEAD(&md->md_pktbuf, pf_list);
-      mtx_unlock(&md->md_mtx);
-      return p;
+	struct packet *p;
+
+	mtx_lock(&md->md_mtx);
+	p = STAILQ_FIRST(&md->md_pktbuf);
+	if (p == NULL){
+		mtx_unlock(&md->md_mtx);
+		return NULL;
+	}
+
+	STAILQ_REMOVE_HEAD(&md->md_pktbuf, pf_list);
+	mtx_unlock(&md->md_mtx);
+	return p;
 }
 
-void medium_open(struct wtap_medium *md)
+void
+medium_open(struct wtap_medium *md)
 {
-      mtx_lock(&md->md_mtx);
-      md->open = 1;
-      mtx_unlock(&md->md_mtx);
+	mtx_lock(&md->md_mtx);
+	md->open = 1;
+	mtx_unlock(&md->md_mtx);
 }
 
-void medium_close(struct wtap_medium *md)
+void
+medium_close(struct wtap_medium *md)
 {
-      mtx_lock(&md->md_mtx);
-      md->open = 0;
-      mtx_unlock(&md->md_mtx);
+	mtx_lock(&md->md_mtx);
+	md->open = 0;
+	mtx_unlock(&md->md_mtx);
 }
--- src/if_medium.h
+++ src/if_medium.h
@@ -32,38 +32,41 @@
  * $FreeBSD$
  */
 
-#ifndef _DEV_WTAP_MEDIUM_H
-#define _DEV_WTAP_MEDIUM_H
+#ifndef	__DEV_WTAP_MEDIUM_H__
+#define	__DEV_WTAP_MEDIUM_H__
 
 #include "if_wtapvar.h"
 #include "wtap_hal/handler.h"
 
 struct packet {
-    STAILQ_ENTRY(packet)	pf_list;
-    struct mbuf 		*m;
-    int				id;
+	STAILQ_ENTRY(packet)	pf_list;
+	struct mbuf *		m;
+	int			id;
 };
 typedef STAILQ_HEAD(, packet) md_pkthead;
 
 struct wtap_medium {
-    struct mtx 			md_mtx;
-    //int				visibility[MAX_NBR_WTAP];
-    //struct stailhead 		*headp; 
-    //packet_head			pktbuf;
-    //STAILQ_HEAD(stailhead, packet) pktbuf;
-    //STAILQ_HEAD(stailhead, packet) pktbuf;
-	// = STAILQ_HEAD_INITIALIZER(head);
-    int				open;		/* 0 means we drop packets, 1 we queue them */
-    md_pkthead			md_pktbuf;	/* master queue */
-    struct eventhandler 	*tx_handler;
-    struct timehandler		*bc_handler;
+	struct mtx 			md_mtx;
+#if 0
+	int				visibility[MAX_NBR_WTAP];
+	struct stailhead 		*headp;
+	packet_head			pktbuf;
+	STAILQ_HEAD(stailhead, packet) pktbuf;
+	STAILQ_HEAD(stailhead, packet) pktbuf;
+	/* = STAILQ_HEAD_INITIALIZER(head); */
+#endif
+	/* 0 means we drop packets, 1 we queue them */
+	int				open;
+	md_pkthead			md_pktbuf;	/* master queue */
+	struct eventhandler		*tx_handler;
+	struct timehandler		*bc_handler;
 };
 
-void init_medium(struct wtap_medium *);
-void deinit_medium(struct wtap_medium *);
-void medium_open(struct wtap_medium *);
-void medium_close(struct wtap_medium *);
-int medium_transmit(struct wtap_medium *, int id, struct mbuf*);
-struct packet *medium_get_next_packet(struct wtap_medium *);
+extern	void init_medium(struct wtap_medium *);
+extern	void deinit_medium(struct wtap_medium *);
+extern	void medium_open(struct wtap_medium *);
+extern	void medium_close(struct wtap_medium *);
+extern	int medium_transmit(struct wtap_medium *, int id, struct mbuf*);
+extern	struct packet *medium_get_next_packet(struct wtap_medium *);
 
-#endif
+#endif	/* __DEV_WTAP_MEDIUM_H__ */
